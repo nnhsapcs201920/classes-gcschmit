0aa7f1777243a77a15111770f339c1141a414828
Geoff Schmit <gcschmit@gmail.com>
2019-09-09 11:25:58 -0500

    declared and documented methods for the Car class

---
+++ Class Notes/Car.java
@@ -0,0 +1,70 @@
+
+/**
+ * This class models a car.
+ *
+ * @author gcschmit
+ * @version 9 September 2019
+ */
+public class Car
+{
+    /*
+     * When defining a method, specify:
+     *      the visibility (e.g., public)
+     *      the return type (e.g., void)
+     *      the method name (e.g., drive)
+     *      the parameters and their types (e.g., distance of type double)
+     */
+    
+    /**
+     * Account that this car has driven the specified distance, consuming fuel.
+     * 
+     * @param distance  the distance, in miles, this car drove
+     */
+    public void drive(double distance)
+    {
+    }
+    
+    /**
+     * Adds the specified amount of fuel to this car's tank.
+     * 
+     * @param amount    the amount of fuel, in gallons, to add to this car's tank
+     */
+    public void addFuel(double amount)
+    {
+    }
+    
+    /**
+     * Returns the amount of fuel in this car's tank.
+     * 
+     * @return the amount of fuel, in gallons, in this car's tank.
+     */
+    public double getFuelInTank()
+    {
+        return 0.0;
+    }
+    
+    /**
+     * Returns the vehicle identification (VIN) of this car.
+     * 
+     * @return the vehicle identification (VIN) of this car
+     */
+    public String getVIN()
+    {
+        return "";
+    }
+    
+    /**
+     * Sets the vehicle identification (VIN) of this car.
+     * 
+     * @param newVIN    the vehicle identification (VIN) of this car
+     */
+    public void setVIN(String newVIN)
+    {
+    }
+}
+
+
+
+
+
+
