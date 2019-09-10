e85113cc6da6b50f15a44ee96cd6ec1696cc3b26
Geoff Schmit <gcschmit@gmail.com>
2019-09-10 13:21:45 -0500

    declared instance variables and constructors; started unit test

--- Class Notes/Car.java
+++ Class Notes/Car.java
@@ -8,7 +8,55 @@
 public class Car
 {
     /*
-     * When defining a method, specify:
+     * 2. Define the instance variables (store the object's attributes):
+     *      specify the visibility (e.g., private)
+     *          public: accessible by any code in any class
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double)
+     *      specify the name (e.g., fuelEfficiency)
+     *   
+     *   Instance variables differ from local variables in the following ways:
+     *      scoped to the class (accessible in all methods of the class;
+     *          lifetime is the same as the object)
+     *      automatically initialized to a default value (0, false, null)
+     *      best practice is not to immediately initialize instance variables
+     */
+    private double fuelEfficiency;  // in units of miles per gallon
+    private double fuelInTank;  // in units of gallons
+    private String vin; // vehicle identification
+    
+    
+    /*
+     * 3. Define the constructor(s):
+     *      responsible for initializing newly created objects
+     *      invoked automatically via the new operator
+     *      name of the constructor must match the name of the class
+     *      has no return type (not even void)
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another constructor (with restrictions)
+     */
+    
+    /**
+     * Default constructor for the Car class.
+     *      Initializes this car's fuel efficiency to 30 mpg, the fuel in the tank to 0 gallons,
+     *          and the VIN to null.
+     */
+    public Car()
+    {
+    }
+    
+    /**
+     * Constructs a new Car object with the specified fuel efficiency.
+     * 
+     * @param initialFuelEfficiency the initial fuel efficiency, in miles per gallon, of this new car
+     */
+    public Car(double initialFuelEfficiency)
+    {
+    }
+    
+    
+    /*
+     * 1. Define methods by specifying:
      *      the visibility (e.g., public)
      *      the return type (e.g., void)
      *      the method name (e.g., drive)
