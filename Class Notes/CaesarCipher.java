59536dd7ad2516c92d83ec3e7781f9d2cd86a33c
Geoff Schmit <gcschmit@gmail.com>
2019-09-24 09:31:05 -0500

    explored static and final keywords

--- Class Notes/CaesarCipher.java
+++ Class Notes/CaesarCipher.java
@@ -10,6 +10,19 @@ import java.util.Scanner;
  */
 public class CaesarCipher
 {
+    /*
+     * static: one value for the variable for all objects of the class. 
+     *      This is like class attributes in Python.
+     *      Static class variables can be accessed directly through the class
+     *          (e.g., CaesarCipher.ALPHABET, Math.PI, Color.RED)
+     *  
+     *  String literal
+     *      is an instance of the String class (not a primitives) delinated by double quotes and
+     *          must be defined on a single line
+     *  
+     *  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" is a string literal equivalent to:
+     *      new String("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+     */
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     public static void main(String[] args)
@@ -63,6 +76,38 @@ public class CaesarCipher
          */
         int secondsPerGuess = s.nextInt();
         
+    }
+    
+    
+    /**
+     * Formats the average time to crack the cipher based on the specified number of seconds and
+     *      displays via System.out in several formats.
+     *      
+     *  This method is static and, therefore, is independent of the state of a CaesarCipher object.
+     *      As a result, this method may be invoked on the class instead of a variable that referenes
+     *      an object (e.g,, CaesarCipher.printAverageTimeToCrack(10000000);)
+     *      In addition, this method cannot access any instance variables.
+     *      
+     *  @param totalSeconds     the average number of seconds to crack the cipher
+     */
+    public static void printAverageTimeToCrack(long totalSeconds)
+    {
+        /*
+         * Instead of using a "magic number" (e.g., 3.14159), use constants defined by us or
+         *      Java Standard Library. For example, in the Math class is defined:
+         *      
+         *      public static final double PI = 3.141592654;
+         *      
+         *  Declare a constant with the final keyword.
+         *      By convention, constants are in all caps with underscores.
+         */
+        final int SECONDS_FOR_EVERY_MINUTE = 60;
+        final int MINUTES_FOR_EVERY_HOUR = 60;
+        final int HOURS_FOR_EVERY_DAY = 24;
+        final int DAYS_FOR_EVERY_YEAR = 365;
+        
+        // if we try to change the value, a compiler error will be generated
+        //SECONDS_FOR_EVERY_MINUTE = 30;
     }
     
     
